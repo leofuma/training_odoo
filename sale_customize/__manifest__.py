cd53883798200fc592065bec47afcf9b1ec0bd66
leofuma <leofuma@gmail.com>
2021-10-08 19:20:36 -0400

    1.1.3

--- sale_customize/__manifest__.py
+++ sale_customize/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Tool',
-    'version': '0.1',
+    'version': '1.1.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale_management'],
